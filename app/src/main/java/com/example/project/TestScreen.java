1353fa7d4edefce5a117775750252f965a0e7492
Tan <nguyenhuytan2004@gmail.com>
2025-02-28 23:25:28 +0700

    add note screen

---
+++ app/src/main/java/com/example/project/TestScreen.java
@@ -0,0 +1,12 @@
+package com.example.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.note);
+    }
+}
