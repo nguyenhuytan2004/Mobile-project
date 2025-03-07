f513b7b51cd1bf385d4374b2f86686294a5daa4f
Tan <nguyenhuytan2004@gmail.com>
2025-03-07 14:35:46 +0700

    add -calendar- screen

--- app/src/main/java/com/example/project/Calendar.java
+++ app/src/main/java/com/example/project/Calendar.java
@@ -1,11 +1,9 @@
 package com.example.project;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-
-
-public class TestScreen extends Activity {
+public class Calendar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
