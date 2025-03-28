97887a0c9a885b41fd2430096af530590e2dfd7f
xanh <nvxanh75@gmail.com>
2025-03-28 10:19:30 +0700

    test screen change

--- app/src/main/java/com/example/project/TestScreen.java
+++ app/src/main/java/com/example/project/TestScreen.java
@@ -9,6 +9,6 @@ public class TestScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_calendar);
+        setContentView(R.layout.notify_setting_daily);
     }
 }
